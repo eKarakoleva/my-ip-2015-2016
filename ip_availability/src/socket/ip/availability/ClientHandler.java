948dd1347e62df9a5ca74bec40e9afcd4dd1c172
Elena <Elena@none>
2015-11-12 14:44:15 +0200

    Add stopServer functionality

--- ip_availability/src/socket/ip/availability/ClientHandler.java
+++ ip_availability/src/socket/ip/availability/ClientHandler.java
@@ -5,32 +5,37 @@ import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ClientHandler {
-	
-	private final Socket socket;
+	public class ClientHandler {
+		private static final String COMMAND_STOP_SERVER = "stopServer";
 
-	public ClientHandler(Socket socket) {
-		this.socket = socket;
-	}
-	
-	public void run() {
-		try {
-			final PrintStream out = 
-				new PrintStream(socket.getOutputStream());
-			
-			final Scanner scanner =
-				new Scanner(socket.getInputStream());
-			
-			while (scanner.hasNextLine()) {
-				final String command = scanner.nextLine();
-				
-				out.println(command);
+		private final Socket socket;
+
+		private final EchoServer echoServer;
+
+		public ClientHandler(EchoServer echoServer, Socket socket) {
+			this.socket = socket;
+			this.echoServer = echoServer;
+		}
+		
+		public void run() {
+			try {
+				final PrintStream out = 
+					new PrintStream(socket.getOutputStream());
+				final Scanner scanner =
+					new Scanner(socket.getInputStream());
+				while (scanner.hasNextLine()) {
+					final String command = scanner.nextLine();
+					if (COMMAND_STOP_SERVER.equals(command)) {
+						echoServer.stopServer();
+						break;
+					}
+					out.println(command);
+				}
+				scanner.close();
+				out.close();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
-			scanner.close();
-			out.close();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 	}
 
-}
